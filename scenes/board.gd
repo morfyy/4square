798c556990fbd7aff5bb349cd37d74a048b1dbde
Morfy <morfyy1313@gmail.com>
2026-01-27 21:39:57 +0100

    feat: Implement board

---
+++ scenes/board.gd
@@ -0,0 +1,22 @@
+extends NinePatchRect
+class_name Board
+
+var hole_pck:PackedScene = preload("res://scenes/hole.tscn")
+
+
+func _ready() -> void:
+	generate(Vector2i(6,6))
+
+func generate(gridsize:Vector2i) -> void:
+	var hole_size:Vector2 = Vector2(size.x/gridsize.x, size.y/gridsize.y)
+	
+	for child in get_children():
+		child.queue_free()
+	
+	for x in range(gridsize.x):
+		for y in range(gridsize.y):
+			var inst:Hole = hole_pck.instantiate()
+			inst.size = hole_size
+			inst.position.x = x*hole_size.x
+			inst.position.y = y*hole_size.y
+			add_child(inst)
