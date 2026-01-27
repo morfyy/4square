3f490958c994a5f161943f84d41c7a22fc4e1ba9
Morfy <morfyy1313@gmail.com>
2026-01-27 22:28:04 +0100

    feat: Add simple visuals.

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -8,5 +8,5 @@ var game:PackedScene = preload("res://scenes/game.tscn")
 func _on_start_pressed() -> void:
 	var new_game:Game = game.instantiate()
 	get_tree().root.add_child(new_game)
-	new_game.create(Vector2i(6,6))
-	hide()
+	new_game.create(Vector2i(5,5))
+	queue_free()
