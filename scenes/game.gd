88bc3db57cf2f649d1333bca361c2ff6f9cc393e
Morfy <morfyy1313@gmail.com>
2026-01-27 22:07:26 +0100

    feat: Add menu

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,2 +1,8 @@
 extends Control
 class_name Game
+
+@onready var board:Board = $board/Board
+
+
+func create(gridsize:Vector2i) -> void:
+	board.generate(gridsize)
---
+++ scenes/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var game:PackedScene = preload("res://scenes/game.tscn")
+
+
+
+
+func _on_start_pressed() -> void:
+	var new_game:Game = game.instantiate()
+	get_tree().root.add_child(new_game)
+	new_game.create(Vector2i(6,6))
+	hide()
